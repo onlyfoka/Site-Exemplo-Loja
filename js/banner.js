aac758f3aba5b2dae0993553f64295c58c9a24b5
Foka <onlyfoka@yahoo.com>
2025-08-02 10:47:18 -0300

    Commit inicial

---
+++ js/banner.js
@@ -0,0 +1,12 @@
+var banners = ["img/destaque-home.png","img/destaque-home-2.png"];
+var bannerAtual = 0;
+
+function trocaBanner(){
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector ('.banner-destaque img').src = banners[bannerAtual]
+    console.log("teste")
+}
+
+setInterval(trocaBanner, 4000)
+
+
---
+++ js/home.js
@@ -0,0 +1,12 @@
+$('.novidades button').click(function() {
+    $('novidades').addClass('painel-aberto');
+});
+$('.novidades button').click(function(){
+    $('.novidades').toggleClass('painel-aberto')
+});
+$('.mais-vendidos button').click(function() {
+    $('mais-vendidos').addClass('painel-aberto');
+});
+$('.mais-vendidos button').click(function(){
+    $('.mais-vendidos').toggleClass('painel-aberto')
+});
